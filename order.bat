4ce76b5e9b041b438bb7297d77c22b9f6d0cf4ea
ag0n1k <ag0n1kness@gmail.com>
2017-10-04 17:49:41 +0300

    Added new formats

--- order.bat
+++ order.bat
@@ -1,4 +1,4 @@
-rem Проверяем существование и создаем необходимые каталоги
+rem РџСЂРѕРІРµСЂСЏРµРј СЃСѓС‰РµСЃС‚РІРѕРІР°РЅРёРµ Рё СЃРѕР·РґР°РµРј РЅРµРѕР±С…РѕРґРёРјС‹Рµ РєР°С‚Р°Р»РѕРіРё
 if not exist "archives" md "archives"
 if not exist "iso" md "iso"
 if not exist "music" md "music"
@@ -9,33 +9,46 @@ if not exist "images" md "images"
 if not exist "docs" md "docs"
 if not exist "html" md "html"
 if not exist "books" md "books"
+if not exist "rpm" md "rpm"
+if not exist "certs" md "certs"
+if not exist "traffic" md "traffic"
+if not exist "tech_docs" md "tech_docs"
 
-rem Перемещаем архивы часто используемых типов в arhives
-for %%f in (*.zip,*.gz,*.tgz,*.rar) do move "%%f" "archives"
+rem РџРµСЂРµРјРµС‰Р°РµРј Р°СЂС…РёРІС‹ С‡Р°СЃС‚Рѕ РёСЃРїРѕР»СЊР·СѓРµРјС‹С… С‚РёРїРѕРІ РІ arhives
+for %%f in (*.zip,*.gz,*.tgz,*.rar,*.bz2) do move "%%f" "archives"
 
-rem ISO-файлы — в каталог ISO
+rem ISO-С„Р°Р№Р»С‹ вЂ” РІ РєР°С‚Р°Р»РѕРі ISO
 move *.iso iso
 
-rem Видео — в каталог video
+rem Р’РёРґРµРѕ вЂ” РІ РєР°С‚Р°Р»РѕРі video
 for %%f in (*.avi,*.mov,*.mp4,*.mkv,*.3gp) do move "%%f" "video"
 
-rem Музыка (в основном это MP3-файлы)
+rem РњСѓР·С‹РєР° (РІ РѕСЃРЅРѕРІРЅРѕРј СЌС‚Рѕ MP3-С„Р°Р№Р»С‹)
 move *.mp3 music
 
-rem Программы и фото
+rem РџСЂРѕРіСЂР°РјРјС‹ Рё С„РѕС‚Рѕ
 
 for %%f in (*.exe,*.msi,*.jar) do move "%%f" "programs"
 move *.jpg photos
 
-rem Изображения
+rem РР·РѕР±СЂР°Р¶РµРЅРёСЏ
 for %%f in (*.png,*.bmp,*.gif,*.tiff) do move "%%f" "images"
 
-rem Документы
-for %%f in (*.txt,*.doc,*.docx,*.xls,*.pptx,*.ppt,*.rtf,*.csv) do move "%%f" "docs"
+rem Р”РѕРєСѓРјРµРЅС‚С‹
+for %%f in (*.txt,*.doc,*.docx,*.xls,*.pptx,*.ppt,*.rtf,*.csv,*.xlsx,*.xlsm) do move "%%f" "docs"
 
 for %%f in (*.pdf,*.djvu) do move "%%f" "books"
 
+for %%f in (*.cer,*.der,*.csr) do move "%%f" "certs"
+
+for %%f in (*.mib,*.cap,*.pcap) do move "%%f" "traffic"
+
+for %%f in (*.xml, *.json, *.log, *.sh) do move "%%f" "tech_docs"
+
 move *.html html
 
-rem Удаляем торрент-файлы
+for %%f in (*.rpm, *.whl) do move "%%f" rpm
+
+rem РЈРґР°Р»СЏРµРј С‚РѕСЂСЂРµРЅС‚-С„Р°Р№Р»С‹
 del *.torrent
+del *.jnlp
